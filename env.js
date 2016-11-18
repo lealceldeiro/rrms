9f4fce4e9b4e9be156ddd6d9b5c213227bec7b2d
Asiel <lealceldeiro@gmail.com>
2016-11-18 12:51:17 -0500

    Indentation to config file.

--- env.js
+++ env.js
@@ -4,16 +4,23 @@
 
 (
     function (window) {
-    window.__env = window && window.__env ? window.__env : {};
 
-    // API url
-    window.__env.apiUrl = '<your_absoute_api_restURL_here>';
+        /**
+         * Configuration options
+         **/
 
-    // Base url
-    window.__env.baseUrl = '<your_absoute_baseUrl_here>';
+        window.__env = window && window.__env ? window.__env : {};
 
-    // Whether or not to enable debug mode
-    // Setting this to false will disable console output
-    window.__env.enableDebug = true;
-}
+        // API url
+        window.__env.apiUrl = '<your_absoute_api_restURL_here>';
+
+        // Base url
+        window.__env.baseUrl = '<your_absoute_baseUrl_here>';
+
+        // Whether or not to enable debug mode
+        // Setting this to false will disable console output
+        window.__env.enableDebug = true;
+
+
+    }
 )(this);
