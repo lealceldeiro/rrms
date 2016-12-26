514ff1e607215fd739c654d9eeb88c902e15c655
Asiel <lealceldeiro@gmail.com>
2016-12-26 13:19:29 -0500

    Util - Added support for array evaluation in nNNnN function in util service

--- client/app/modules/_common/js/util/value.srv.js
+++ client/app/modules/_common/js/util/value.srv.js
@@ -14,7 +14,15 @@
 
             return self.service;
 
+            /**
+             * Return whether a specific field is valid or not
+             * @param val Field to be evaluated
+             * @returns {boolean} True if <code>f</code> is not <code>null</code> and is not <code>undefined</code>
+             */
             function fnNNnN (val){
+                if (angular.isArray(val)) {
+                    return typeof val !== 'undefined' && val !== null && val.length > 0;
+                }
                 return typeof val !== 'undefined' && val !== null;
             }
         };
